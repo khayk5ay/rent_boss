599db6bfccfcf82c8b6d54eb8218342998a26ed1
Kaine Victoria Karibi-George <kaine.vic.kg@gmail.com>
2023-11-28 15:09:56 +0100

    Specify datatype of some columns when loading csv files to database

--- scripts/load.py
+++ scripts/load.py
@@ -6,7 +6,7 @@ This script uploads the files in sql tables
 import os
 from dotenv import load_dotenv
 from my_sql_engine import Engine
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, DateTime, String
 import pandas as pd
 #import datetime
 
@@ -25,7 +25,7 @@ def csv_to_sql():
             table_name = file.split(".")[0]
             df = pd.read_csv(f"{base_path}/data/{file}")
             df.to_sql(name = table_name, con= connection, if_exists="replace",
-                      dtype={"last_avail_check": str, "last_updated": str,  "last_ratings":"DATETIME"})
+                      dtype={"airbnb_id": String(64), "last_avail_check": DateTime, "last_updated": DateTime,  "last_ratings":DateTime})
 
 
 
