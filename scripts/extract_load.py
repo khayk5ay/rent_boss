3d80c0efb4dafcbf3a3ebbe411acc996377e3e9f
Kaine Victoria Karibi-George <kaine.vic.kg@gmail.com>
2023-12-06 12:31:06 +0100

    Defined the Listings Review table using slqalchemy.orm

--- scripts/extract_load.py
+++ scripts/extract_load.py
@@ -4,7 +4,28 @@ However, I want to use the declarative base class of SQLAlchemy to insert the re
 As opposed to using the pandas to_sql method like i did in the other set of data
 """
 
-from SQLAlchemy import Declarative_Base
+from my_sql_engine import Engine, Base
+from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
 import os
 from dotenv import load_dotenv
 
+
+engine_object = Engine("rentboss")
+
+engine = engine_object.make_engine()
+
+session = Session(engine)
+
+
+class ListingReviews(Base):
+    __tablename__ = "listing_reviews"
+    review_id = Column(Integer, primary_key = True)
+    comments = Column(String(555))
+    response = Column(String(255))
+    date_time = Column(DateTime)
+    language = Column(String(8))
+    rating = Column(Integer)
+    guest_id = Column(Integer)
+    
+    
