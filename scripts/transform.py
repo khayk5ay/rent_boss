299a70af0970711b937546c3ee136323b4571e76
Kaine Victoria Karibi-George <kaine.vic.kg@gmail.com>
2024-01-23 17:00:06 +0100

    Transform json documents using pandas

--- scripts/transform.py
+++ scripts/transform.py
@@ -18,17 +18,37 @@ def admin_info_transform():
 
     df = pd.read_json(file_path)
     df.admin1_name = np.where(df["admin1_name"] == "n/a", df["admin1"], df["admin1_name"])
-    pass
+    df = df.drop_duplicates()
+    
 
 def listing_georef_transform():
-    
+
+    file_path = base_path+"/data/listings_georef.json"
+
+    df = pd.read_json(file_path)
+    df = df.drop_duplicates()
     pass
 
-def listing_status_transform():
-    
+def listing_reviews_transform():
+    file_path = base_path+"/data/listing_reviews.json"
+
+    df = pd.read_json(file_path)
+    df = df.drop_duplicates()
+
+    # Remove the unwanted quotations marks surrounding the text
+    df.comments = df.comments.str[1:-1]
+    df.response = df.response.str[:-1]
+    df.response = df.response.str[1:]
+
     pass
 
+def listing_descriptions_transform():
+    file_path = base_path+"/data/listing_descriptions.json"
 
+    df = pd.read_json(file_path)
+    df = df.drop_duplicates()
+    
+    pass
 
 def main():
     admin_info_transform()
